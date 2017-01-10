b2fe1a5525918c8f6488771902aaf1b50f746707
Anmol Pradhan <panmol1700@gmail.com>
2017-01-10 15:37:25 -0500

    fixing rabbitmq setup, change signing key and adding rabbitmq management

--- scripts/rabbitmq.sh
+++ scripts/rabbitmq.sh
@@ -1,13 +1,18 @@
 #!/usr/bin/env bash
 
-echo ">>> Installing RabbitMQ"
+echo ">>> talling RabbitMQ"
 
 apt-get -y install erlang-nox
-wget http://www.rabbitmq.com/rabbitmq-signing-key-public.asc
-apt-key add rabbitmq-signing-key-public.asc
+wget http://www.rabbitmq.com/rabbitmq-release-signing-key.asc
+apt-key add rabbitmq-release-signing-key.asc
 echo "deb http://www.rabbitmq.com/debian/ testing main" > /etc/apt/sources.list.d/rabbitmq.list
 apt-get update
-apt-get install rabbitmq-server
+apt-get install -y rabbitmq-server
+rabbitmq-plugins enable rabbitmq_management
+
+# Allow guests to login thru rabbitmq management
+echo "[{rabbit, [{loopback_users, []}]}]." >> /etc/rabbitmq/rabbitmq.config
+service rabbitmq-server restart
 
 rabbitmqctl add_user $1 $2
 rabbitmqctl set_permissions -p / $1 ".*" ".*" ".*"
