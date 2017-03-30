1647a2271a1707cffc437353819fd0122cca7f84
frets1700 <panmol1700@gmail.com>
2017-03-30 14:03:14 -0400

    fixing elastic search version

--- scripts/elasticsearch.sh
+++ scripts/elasticsearch.sh
@@ -5,12 +5,21 @@ echo ">>> Installing Elasticsearch"
 # Set some variables
 ELASTICSEARCH_VERSION=$1 # Check https://www.elastic.co/downloads/elasticsearch for latest version
 
+comp=$(awk 'BEGIN{ print "'$ELASTICSEARCH_VERSION'"<"'5.1.2'" }')
+
 # Install prerequisite: Java
 # -qq implies -y --force-yes
-sudo apt-get update
-sudo apt-get install -qq openjdk-7-jre-headless
+if [ "$comp" -eq 1 ]; then
+	sudo apt-get update
+    sudo apt-get install -qq openjdk-7-jre-headless
+    wget --quiet https://download.elastic.co/elasticsearch/elasticsearch/elasticsearch-$ELASTICSEARCH_VERSION.deb
+else
+	sudo add-apt-repository -y ppa:openjdk-r/ppa
+	sudo apt-get update
+	sudo apt-get -y install openjdk-8-jdk openjdk-8-jre
+	wget --quiet https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-$ELASTICSEARCH_VERSION.deb
+fi
 
-wget --quiet https://download.elastic.co/elasticsearch/elasticsearch/elasticsearch-$ELASTICSEARCH_VERSION.deb
 sudo dpkg -i elasticsearch-$ELASTICSEARCH_VERSION.deb
 rm elasticsearch-$ELASTICSEARCH_VERSION.deb
 
@@ -18,6 +27,19 @@ rm elasticsearch-$ELASTICSEARCH_VERSION.deb
 sudo sed -i "s/# index.number_of_shards: 1/index.number_of_shards: 1/" /etc/elasticsearch/elasticsearch.yml
 sudo sed -i "s/# index.number_of_replicas: 0/index.number_of_replicas: 0/" /etc/elasticsearch/elasticsearch.yml
 sudo sed -i "s/# bootstrap.mlockall: true/bootstrap.mlockall: true/" /etc/elasticsearch/elasticsearch.yml
+
+# either of the next two lines is needed to be able to access "localhost:9200" from the host os
+sudo echo "network.bind_host: 0" >> /etc/elasticsearch/elasticsearch.yml
+sudo echo "network.host: 0.0.0.0" >> /etc/elasticsearch/elasticsearch.yml
+
+# enable cors (to be able to use Sense)
+# sudo echo "http.cors.enabled: true" >> /etc/elasticsearch/elasticsearch.yml
+sudo echo "http.cors.allow-origin: /https?:\/\/localhost(:[0-9]+)?/" >> /etc/elasticsearch/elasticsearch.yml
+
+# enable dynamic scripting
+sudo echo "script.inline: on" >> /etc/elasticsearch/elasticsearch.yml
+sudo echo "script.indexed: on" >> /etc/elasticsearch/elasticsearch.yml
+
 sudo service elasticsearch restart
 
 # Configure to start up Elasticsearch automatically
