57b7b115204324a48e5b9390d48bbf89bca083d3
frets1700 <panmol1700@gmail.com>
2017-09-22 11:01:25 -0400

    fixing kibana script

--- scripts/kibana.sh
+++ scripts/kibana.sh
@@ -1,17 +1,30 @@
 #!/usr/bin/env bash
 
-echo ">>> Installing Kibana"
+if [[ -z $1 ]]; then
+        KIBANA_VERSION="5.2.2"
+else
+        KIBANA_VERSION=$1
+fi
 
-# Set some variables
-KIBANA_VERSION=4.1.1 # Check https://www.elastic.co/downloads/kibana for latest version
+echo ">>> Installing Kibana $KIBANA_VERSION"
 
 sudo mkdir -p /opt/kibana
-wget --quiet https://download.elastic.co/kibana/kibana/kibana-$KIBANA_VERSION-linux-x64.tar.gz
-sudo tar xvf kibana-$KIBANA_VERSION-linux-x64.tar.gz -C /opt/kibana --strip-components=1
-rm kibana-$KIBANA_VERSION-linux-x64.tar.gz
+
+comp=$(awk 'BEGIN{ print "'$KIBANA_VERSION'"<"'4.6.0'" }')
+
+if [ "$comp" -eq 1 ]; then
+    wget --quiet https://download.elastic.co/kibana/kibana/kibana-$KIBANA_VERSION-linux-x64.tar.gz
+    sudo tar xvf kibana-$KIBANA_VERSION-linux-x64.tar.gz -C /opt/kibana --strip-components=1
+    rm kibana-$KIBANA_VERSION-linux-x86_64.tar.gz
+else
+    wget --quiet https://artifacts.elastic.co/downloads/kibana/kibana-$KIBANA_VERSION-linux-x86_64.tar.gz
+    sudo tar xvf kibana-$KIBANA_VERSION-linux-x86_64.tar.gz -C /opt/kibana --strip-components=1
+    rm kibana-$KIBANA_VERSION-linux-x86_64.tar.gz
+fi
 
 # Configure to start up Kibana automatically
-cd /etc/init.d && sudo wget --quiet https://gist.githubusercontent.com/thisismitch/8b15ac909aed214ad04a/raw/bce61d85643c2dcdfbc2728c55a41dab444dca20/kibana4
+cd /etc/init.d
+sudo wget --quiet https://gist.githubusercontent.com/thisismitch/8b15ac909aed214ad04a/raw/bce61d85643c2dcdfbc2728c55a41dab444dca20/kibana4
 
 sudo chmod +x /etc/init.d/kibana4
 sudo update-rc.d kibana4 defaults 95 10
