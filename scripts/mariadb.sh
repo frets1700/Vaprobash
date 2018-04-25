ceb100e588fd07cb7877ac7879a90738848db759
frets1700 <panmol1700@gmail.com>
2018-04-25 18:24:50 -0400

    check mariadb version before installing

--- scripts/mariadb.sh
+++ scripts/mariadb.sh
@@ -4,20 +4,20 @@ echo ">>> Installing MariaDB"
 
 [[ -z $1 ]] && { echo "!!! MariaDB root password not set. Check the Vagrant file."; exit 1; }
 
+# default version
+MARIADB_VERSION='10.1'
+
 version=$(lsb_release -sr)
 if [[ $version == "16.04" ]]; then
 	echo ">>> Ubuntu 16.04 detected..."
 	sudo apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xF1656F24C74CD1D8
-	sudo add-apt-repository -y 'deb [arch=amd64,i386,ppc64el] http://nyc2.mirrors.digitalocean.com/mariadb/repo/10.2/ubuntu xenial main'
+	sudo add-apt-repository -y "deb [arch=amd64,i386,ppc64el] http://nyc2.mirrors.digitalocean.com/mariadb/repo/$MARIADB_VERSION/ubuntu xenial main"
 else
 	sudo apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xcbcb082a1bb943db
 	sudo add-apt-repository "deb [arch=amd64,i386] http://mirrors.accretive-networks.net/mariadb/repo/$MARIADB_VERSION/ubuntu trusty main"
 fi
 
 sudo apt update
-
-# default version
-MARIADB_VERSION='10.1'
 
 # Install MariaDB without password prompt
 # Set username to 'root' and password to 'mariadb_root_password' (see Vagrantfile)
